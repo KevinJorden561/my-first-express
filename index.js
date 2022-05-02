7bfc7f5a3eaeabcc3e8fd947e25e350d782ed81f
KevinJorden561 <kevinjorden513@gmail.com>
2022-05-02 17:07:16 -0400

    express practice

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { hello, helloPerson } from "./src/hello.js";
+import { welcomeHome } from "./src/welcome.js";
+
+const PORT = 3030;
+
+const app = express();
+app.use(express, json());
+
+//routes (or list of allowed requests)
+app.get("/hello", hello);
+
+app.post("/hello", greetNewPerson);
+
+app.get("/", (request, response) => {
+  response.send("These are not the droids you're looking for. 👋🏼");
+});
+
+//below :person is a param that gets attached to the response
+app.get("/hello/:person", helloPerson);
+
+app.get("/", welcomeHome);
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
